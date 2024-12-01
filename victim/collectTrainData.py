b9e7f292207926666638fa7d8a415fbb49edcf09
George Krier <gkrier@colostate.edu>
2024-12-01 16:11:55 -0700

    Collect keylogger data complete

--- victim/collectTrainData.py
+++ victim/collectTrainData.py
@@ -5,8 +5,7 @@ import time
 
 keys_down = []
 
-recorded_keypress = []
-recorded_timestamp = []
+recorded_data = []
 
 cases = ['Rctrl', 'p', 'esc', 'g', 'slash', 'down', '7', 'equal', 'w', 'a', 'dash', 'caps', 'l', 'd', 'backspace', 'bracketclose', 'z', '1', 'end', 'Rshift', 'comma', 'c', 'tab', 'b', 'j', 'right', 'Lctrl', 'n', 't', 'f', 'm', 'o', 'apostrophe', 'y', '8', 'space', 'backslash', 's', '9', 'i', 'r', 'bracketopen', 'semicolon', 'q', '5', 'k', '3', 'x', '4', '6', '2', 'Lshift', 'left', 'backtick', 'enter', 'fullstop', 'e', '0', 'h', 'v', 'up', 'u', 'delete']
 mapped_cases = ['right ctrl', 'p', 'esc', 'g', '/', 'down', '7', '=', 'w', 'a', '-', 'caps lock', 'l', 'd', 'backspace', ']', 'z', '1', 'end', 'right shift', ',', 'c', 'tab', 'b', 'j', 'right', 'ctrl', 'n', 't', 'f', 'm', 'o', '\'', 'y', '8', 'space', '\\', 's', '9', 'i', 'r', '[', ';', 'q', '5', 'k', '3', 'x', '4', '6', '2', 'shift', 'left', '`', 'enter', '.', 'e', '0', 'h', 'v', 'up', 'u', 'delete']
@@ -16,9 +15,9 @@ def on_event(key):
     if key.event_type == "down" and actual_key not in keys_down:
         if actual_key in mapped_cases:
             print(actual_key)
+            print(datetime.datetime.fromtimestamp(key.time))
             keys_down.append(actual_key)
-            recorded_keypress.append(cases[mapped_cases.index(actual_key)])
-            recorded_timestamp.append(key.time)
+            recorded_data.append({"keypress": cases[mapped_cases.index(actual_key)], "time": key.time})
             
     if key.event_type == "up" and actual_key in mapped_cases:
         try:
@@ -31,10 +30,16 @@ keyboard.hook(on_event)
 if not os.path.exists("./victim/output"):
     os.mkdir("./victim/output")
 
-while True:
-    time.sleep(10)
-    print(recorded_keypress)
-    print(recorded_timestamp)
-    print(len(recorded_keypress))
-    print(len(recorded_timestamp))
-    
+try:
+    while True:
+        time.sleep(5)
+        # print(recorded_data)
+        # print(len(recorded_data))
+except KeyboardInterrupt:
+    keyboard.unhook(on_event)
+    dt = datetime.datetime.now()
+    filename = f"./victim/output/{dt.date()}-{dt.timestamp()}.txt"
+    with open(filename, "w") as file:
+        for entry in recorded_data:
+            file.write(f"{entry["keypress"]},{entry["time"]}\n")
+    print(f"Wrote to file: {filename}")
