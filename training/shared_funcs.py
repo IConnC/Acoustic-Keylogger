38c45f1f7c08134ace4fdbfb1100ce83b810cae0
George Krier <George.Krier@colostate.edu>
2024-12-04 21:14:47 -0700

    Modified training and shared_funcs for optimization

--- training/shared_funcs.py
+++ training/shared_funcs.py
@@ -1,10 +1,9 @@
-import tensorflow as tf
-
-ALL_LABELS = tf.constant(['Rctrl', 'p', 'esc', 'g', 'slash', 'down', '7', 'equal', 'w', 'a', 'dash', 'caps', 'l', 'd', 'backspace', 'bracketclose', 'z', '1', 'end', 'Rshift', 'comma', 'c', 'tab', 'b', 'j', 'right', 'Lctrl', 'n', 't', 'f', 'm', 'o', 'apostrophe', 'y', '8', 'space', 'backslash', 's', '9', 'i', 'r', 'bracketopen', 'semicolon', 'q', '5', 'k', '3', 'x', '4', '6', '2', 'Lshift', 'left', 'backtick', 'enter', 'fullstop', 'e', '0', 'h', 'v', 'up', 'u', 'delete'], dtype=tf.string)
-
+def GET_ALL_LABELS(tf):
+    return tf.constant(['Rctrl', 'p', 'esc', 'g', 'slash', 'down', '7', 'equal', 'w', 'a', 'dash', 'caps', 'l', 'd', 'backspace', 'bracketclose', 'z', '1', 'end', 'Rshift', 'comma', 'c', 'tab', 'b', 'j', 'right', 'Lctrl', 'n', 't', 'f', 'm', 'o', 'apostrophe', 'y', '8', 'space', 'backslash', 's', '9', 'i', 'r', 'bracketopen', 'semicolon', 'q', '5', 'k', '3', 'x', '4', '6', '2', 'Lshift', 'left', 'backtick', 'enter', 'fullstop', 'e', '0', 'h', 'v', 'up', 'u', 'delete'], dtype=tf.string)
 
 # Encodes a list of instance labels into a binary vector
-def multi_label_binary_encode_tensor(instance_labels):
+def multi_label_binary_encode_tensor(tf, instance_labels):
+    ALL_LABELS = GET_ALL_LABELS(tf)
     # Ensure instance_labels is a list (to handle both single and multi-label cases)
     if isinstance(instance_labels, tf.Tensor) and instance_labels.shape == ():
         instance_labels = tf.expand_dims(instance_labels, axis=0)
@@ -31,12 +30,13 @@ def multi_label_binary_encode_tensor(instance_labels):
     
     return binary_vector
 
-def multi_label_binary_decode_tensor(binary_vector):
-    binary_vector = binary_vector.numpy()
+def multi_label_binary_decode_tensor(tf, binary_vector):
+    # binary_vector = binary_vector.numpy()
+    ALL_LABELS = GET_ALL_LABELS(tf)
     decoded_labels = [ALL_LABELS[i] for i, val in enumerate(binary_vector) if val == 1]
     return decoded_labels
 
-def get_waveform(filepath):
+def get_waveform(tf, filepath):
     audio_binary = tf.io.read_file(filepath)
     audio = tf.squeeze(audio_binary)
     waveform, samplerate = tf.audio.decode_wav(audio)
@@ -49,13 +49,13 @@ def get_waveform(filepath):
     
     return waveform
 
-def split_into_windows(waveform, frame_length=2205, frame_step=1102): # 50ms windows with 50% overlap
+def split_into_windows(tf, waveform, frame_length=2205, frame_step=1102): # 50ms windows with 50% overlap
     # print("Waveform shape before framing:", waveform.shape)
     frames = tf.signal.frame(waveform, frame_length=frame_length, frame_step=frame_step)
     # print("Frames shape after framing (with overlap):", frames.shape)
     return frames
 
-def split_into_sequences(frames, sequence_length=3):
+def split_into_sequences(tf, frames, sequence_length=3):
     num_frames = tf.shape(frames)[0]
     sequence_step = 1
     start_indices = tf.range(0, num_frames - sequence_length + 1, sequence_step)
